d4f054d9a9d5f58fd53daa7d08065ef66db97c0b
Yulibo123 <1789390420@qq.com>
2020-12-25 10:40:06 +0800

    提交1

--- src/main/java/com/qf/mapper/UserMapper.java
+++ src/main/java/com/qf/mapper/UserMapper.java
@@ -4,9 +4,13 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.qf.pojo.User;
 import org.springframework.stereotype.Repository;
 
+import java.util.Map;
+
 @Repository
 public interface UserMapper extends BaseMapper<User> {
 
     User selectUserByEmailAndPassword(User user);
+    public Integer updateByEmail(Map map);
 
+    public Integer updateByEmail2(User user);
 }
--- src/main/java/com/qf/service/UserService.java
+++ src/main/java/com/qf/service/UserService.java
@@ -2,8 +2,14 @@ package com.qf.service;
 
 import com.qf.pojo.User;
 
+import java.util.Map;
+
 public interface UserService {
 
     User selectUserByEmailAndPassword(User user);
+    public User findByemail(String email);
 
+    public Integer updateByEmail(Map map);
+
+    public Integer updateByEmail2(User user);
 }
